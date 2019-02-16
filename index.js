162abc55d4f870548f27d3e7fd9f48f5c1832dfb
Zack A Claar <claar.zack@gmail.com>
2019-02-16 19:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function takeANumber(customerQueue, customerName) {
   
   return "Welcome, " + customerName + ". You are number " + customerQueue.length + " in line.";
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    for (i = 0; i < katzDeliLine.length; i++) {
+      var currentCustomer = katzDeliLine[i];
+      
+    }
+  }
+}
