36cd80798b57211e6591483a1a45693d23ed5949
Zack A Claar <claar.zack@gmail.com>
2019-02-16 19:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function nowServing(katzDeliLine) {
     if (katzDeliLine.length === 0) {
       return "There is nobody waiting to be served!";
     } else {
-      
+      return katzDeliLine[i];
+      katzDeliLine.shift();
+      //use .shift()
     }
   }
 }
